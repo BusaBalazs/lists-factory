1808fc60b117c23553dbed8f8a1bde41857eb010
BusaBalazs <fishbalazs@gmail.com>
2023-11-07 21:47:38 +0100

    save data to local storage

--- src/components/lists/Items.js
+++ src/components/lists/Items.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
+import { v4 as uuidv4 } from "uuid";
 
 const Items = () => {
+  const text = useRef();
+  const [data, setData] = useState([]);
+
+
+  useEffect(() => {
+    const setData = () => {
+
+      const getData = localStorage.getItem("list") ? JSON.parse(localStorage.getItem("list")) : [];
+
+      console.log(data.id)
+      if (data.id !== undefined) {
+        getData.push(data)
+      }
+      localStorage.setItem("list", JSON.stringify(getData))
+    }
+   
+    return setData(); 
+  }, [data])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setData({
+      id: uuidv4(),
+      name: text.current.value,
+    })
+    
+    text.current.value = ""
+  }
+
   return (
-    <div>Items</div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input ref={text} type="text" />
+        <button>submit</button>
+      </form>
+      <div>
+
+      </div>
+    </>
   )
 }
 
